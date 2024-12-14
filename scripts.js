eb6f30c95b0b0c5c82556604fcc01d601ba8db3c
Johny-Code <jan.tobolewski09@gmail.com>
2024-12-14 16:41:57 +0100

    test firebase 18

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAGZMjOSeECO8i93vOIGYxoT4I2LbmT6I8",
-    authDomain: "lista-prezentowa-marjan.firebaseapp.com",
-    databaseURL: "https://lista-prezentowa-marjan-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "lista-prezentowa-marjan",
-    storageBucket: "lista-prezentowa-marjan.appspot.com",
-    messagingSenderId: "631667451184",
-    appId: "1:631667451184:web:cebd9e98c75f56076054f5",
-    measurementId: "G-D4FE1G6XBN"
-};
+if (!firebase.apps.length) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAGZMjOSeECO8i93vOIGYxoT4I2LbmT6I8",
+        authDomain: "lista-prezentowa-marjan.firebaseapp.com",
+        databaseURL: "https://lista-prezentowa-marjan-default-rtdb.europe-west1.firebasedatabase.app/",
+        projectId: "lista-prezentowa-marjan",
+        storageBucket: "lista-prezentowa-marjan.appspot.com",
+        messagingSenderId: "631667451184",
+        appId: "1:631667451184:web:cebd9e98c75f56076054f5",
+        measurementId: "G-D4FE1G6XBN"
+    };
+    const app = firebase.initializeApp(firebaseConfig);
+    const database = firebase.database();
+}
 
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
 
 function writeTestData() {
     const dbRef = database.ref('test/');
     dbRef.set({
-        message: "Hello from GitHub Pages!"
+        message: "Hello from GitHub Pages! Jan Dupa"
     })
     .then(() => {
-        console.log("Data written successfully.");
+        console.log("Data written successfully. Kupa!");
     })
     .catch((error) => {
         console.error("Error writing data: ", error);
