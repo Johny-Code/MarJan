a45b54cd952ec403fd0a15372d951552dad45b1f
Jan Tobolewski <jan.tobolewski@assecosol.com>
2024-12-19 21:17:59 +0100

    remove api variables 12

---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAGZMjOSeECO8i93vOIGYxoT4I2LbmT6I8',
+    authDomain: 'lista-prezentowa-marjan.firebaseapp.com',
+    databaseURL: 'https://lista-prezentowa-marjan-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: 'lista-prezentowa-marjan',
+    storageBucket: 'lista-prezentowa-marjan.appspot.com',
+    messagingSenderId: '631667451184',
+    appId: '1:631667451184:web:cebd9e98c75f56076054f5',
+    measurementId: 'G-D4FE1G6XBN'
+};
