ccb38e9bd7d51d9f6b3ce0b52cd98f2979e32c2c
Jan Tobolewski <jan.tobolewski@assecosol.com>
2024-12-19 20:30:31 +0100

    remove api variables 4

--- scripts.js
+++ scripts.js
@@ -1,13 +1,4 @@
-export const firebaseConfig = {
-    apiKey: "your_api_key",
-    authDomain: "your_auth_domain",
-    databaseURL: "your_database_url",
-    projectId: "your_project_id",
-    storageBucket: "your_storage_bucket",
-    messagingSenderId: "your_messaging_sender_id",
-    appId: "your_app_id",
-    measurementId: "your_measurement_id"
-};
+import { firebaseConfig } from './scripts/firebaseConfig.js';
 
 // Inicjalizacja Firebase
 const app = firebase.initializeApp(firebaseConfig);
